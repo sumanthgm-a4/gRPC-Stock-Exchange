c8eccdb21c6b19fee4534496e4531dc3393b5d1f
Sumanth GM <214g1a32a4@srit.ac.in>
2026-07-27 21:03:12 +0530

    Added full Add, Delete and Get functionalities

--- stock-trading-server/src/main/java/com/sum/stock_trading_server/service/StockTradingServiceImpl.java
+++ stock-trading-server/src/main/java/com/sum/stock_trading_server/service/StockTradingServiceImpl.java
@@ -1,9 +1,15 @@
 package com.sum.stock_trading_server.service;
 
 import com.sum.stock_trading_server.repository.StockRepository;
+
+import java.util.List;
+
 import org.springframework.grpc.server.service.GrpcService;
 
 import com.sum.stock_trading_server.entity.Stock;
+import com.sum.stock_trading_server.grpc.AddStockRequest;
+import com.sum.stock_trading_server.grpc.Empty;
+import com.sum.stock_trading_server.grpc.StockListResponse;
 import com.sum.stock_trading_server.grpc.StockRequest;
 import com.sum.stock_trading_server.grpc.StockResponse;
 import com.sum.stock_trading_server.grpc.StockTradingServiceGrpc.StockTradingServiceImplBase;
@@ -20,7 +26,57 @@ public class StockTradingServiceImpl extends StockTradingServiceImplBase {
     }
 
     @Override
-    public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+    public void addStock(AddStockRequest request, StreamObserver<Empty> responseObserver) {
+        Stock stockEntity = new Stock();
+
+        stockEntity.setStockSymbol(request.getStockSymbol());
+        stockEntity.setPrice(request.getPrice());
+        
+        stockRepository.save(stockEntity);
+        
+        Empty empty = Empty.newBuilder().build();
+        responseObserver.onNext(empty);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteStock(StockRequest request, StreamObserver<Empty> responseObserver) {
+        Stock stockEntity = stockRepository.findByStockSymbol(request.getStockSymbol());
+
+        stockRepository.delete(stockEntity);
+
+        Empty empty = Empty.newBuilder().build();
+
+        responseObserver.onNext(empty);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllStocks(Empty request, StreamObserver<StockListResponse> responseObserver) {
+        List<Stock> stocks = stockRepository.findAll();
+
+        List<StockResponse> stockResponse = stocks.stream()
+                .map(stock -> {
+                    StockResponse response = StockResponse.newBuilder()
+                            .setStockSymbol(stock.getStockSymbol())
+                            .setPrice(stock.getPrice())
+                            .setTimestamp(stock.getLastUpdated().toString())
+                            .build();
+
+                    return response;
+                })
+                .toList();
+
+        StockListResponse stockListResponse = StockListResponse.newBuilder()
+                .addAllStockList(stockResponse)
+                .build();
+
+        responseObserver.onNext(stockListResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getStock(StockRequest request, StreamObserver<StockResponse> responseObserver) {
         // stock name -> DB -> map response -> return 
 
         String stockSymbol = request.getStockSymbol();
@@ -35,5 +91,4 @@ public class StockTradingServiceImpl extends StockTradingServiceImplBase {
         responseObserver.onNext(stockResponse);
         responseObserver.onCompleted();
     }
-    
 }
